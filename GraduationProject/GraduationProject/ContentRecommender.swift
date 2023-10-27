fd3d62d2fd0ad6086ee3c1fe1b6d2db3d65f4701
a-mystic <totocaca@naver.com>
2023-10-27 19:40:27 +0900

    add emotion(modify).

--- GraduationProject/GraduationProject/ContentRecommender.swift
+++ GraduationProject/GraduationProject/ContentRecommender.swift
@@ -16,8 +16,22 @@ struct ContentRecommender: View {
                     VStack(spacing: 20) {
                         LottieView(jsonName: "bear", loopMode: .repeat(3))
                             .frame(width: 200, height: 200)
-                        Text("애니메이션 감상하는것을 추천드려요.")
-                            .bold()
+                        VStack {
+                            HStack {
+                                Text("분석한 감정: ")
+                                    .bold()
+                                Text("😀")
+                                    .font(.largeTitle)
+                            }
+                            .font(.title)
+                            Text("애니메이션 감상하는것을 추천드려요.")
+                                .bold()
+                        }
+                        .padding()
+                        .background {
+                            RoundedRectangle(cornerRadius: 14)
+                                .foregroundStyle(.blue.opacity(0.4))
+                        }
                         SheepAnimation(width: geometry.size.width, height: geometry.size.height/2)
                     }
                 }
