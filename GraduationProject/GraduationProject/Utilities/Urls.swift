6840c88b7e06d52194fe8245355437289e3c4f72
a-mystic <totocaca@naver.com>
2024-11-08 12:48:34 +0900

    완료

--- GraduationProject/GraduationProject/Utilities/Urls.swift
+++ GraduationProject/GraduationProject/Utilities/Urls.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct ServerUrls {
-    static let base = "https://3d5d-175-205-103-204.ngrok-free.app"
+    static let base = "url"
     static let sentimentValue = "\(base)/sentimentValue"
     static let chat = "\(base)/chat"
     static let check = "\(base)/check"
